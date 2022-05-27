467baedf5ebdaa41344e510538f76ea0231cf4f8
rekxspein <khundrakpamrajesh95@gmail.com>
2022-05-27 08:55:52 +0530

    WorkingApp

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import axios from 'axios'
 import React, { useState } from 'react'
+import { Card} from "react-bootstrap";
 import './App.css'
 
 export default function App() {
-  // const apiKey = '904c55231af7c4945ddfc2dc7a143431'
-  const apikey2 = "eeac188914e521cc6c4c4720d662cce0"
-  const [weatherData, setWeatherData] = useState ([{}])
+  const apikey = "eeac188914e521cc6c4c4720d662cce0"
+  const [weatherData, setWeatherData] = useState ()
   const [city, setCity] = useState("")
 
   const getWeather = (event) => {
-    if (event.key == "Enter") {
-      fetch(`http://api.weatherstack.com/current?access_key=${apikey2}&query=${city}`).then(
-          response => response.json()
-        ).then(
+    if (event.key === "Enter") {
+      fetch(`http://api.weatherstack.com/current?access_key=${apikey}&query=${city}`)
+      .then(response => {console.log(response); return response.json()})
+      .then(
           data => {
             setWeatherData(data)
+            setCity("");
           }
         );
     }
   }
 
   return (
-    <div className='container'>
-      <h4 className='heading'>Weather App</h4>
-      <input 
-        className='input' 
-        placeholder='Type a city..'
-        onChange={e => setCity(e.target.value)}
+    <div className="card">
+      <h4 className="heading">Weather App</h4>
+      <input
+        className="input"
+        placeholder="Type a city.."
+        onChange={(e) => setCity(e.target.value)}
         value={city}
         onKeyDown={getWeather}
       />
-      
+
+      {!weatherData ? (
+        <div>
+          <p className="cityinput">Type a city to start!</p>
+        </div>
+      ) : (
+        <div className="container-fluid px-1 px-md-4 py-5 mx-auto">
+          <div className="flexbox-container">
+            <div className={weatherData.current.temperature > 16 ? "card app warm" : "card app"}>
+              <img src={weatherData.current.weather_icons} alt="weather icon" width="40" height = "40"/>
+              <div className="flexbox-item-1">Location - {weatherData.location.name}</div>
+              <div className="flexbox-item-2">{weatherData.current.weather_descriptions}</div>
+              <div className="flexbox-item-3">{weatherData.current.temperature}&#176;C</div>
+              <div className="">UV index - {weatherData.current.uv_index}</div>
+              <div className="card app history">
+                  <p className='day1'></p>
+                  <p className='day2'></p>
+                  <p className='day3'></p>
+                  <p className='day4'></p>
+                  <p className='day5'></p>
+
+              </div>
+              <div className=""></div>
+              <div className=""></div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
